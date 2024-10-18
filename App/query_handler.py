9ae6a3d311c25d044de22369da064df219112a56
zakariaBensmida <45175094+zakariaBensmida@users.noreply.github.com>
2024-10-18 23:24:51 +0200

    Update query_handler.py

--- App/query_handler.py
+++ App/query_handler.py
@@ -1,7 +1,7 @@
 import os
 from transformers import pipeline
 from typing import List
-from vector_store import VectorStore  # Adjust the import path as necessary
+from .vector_store import VectorStore  # Adjust the import path as necessary
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -33,7 +33,7 @@ class QueryHandler:
 
 # Example usage
 if __name__ == "__main__":
-    from vector_store import VectorStore
+    from .vector_store import VectorStore
 
     # Initialize the vector store (replace with your actual parameters)
     vector_store = VectorStore(store_path='./vector_store/index.faiss', embedding_model_name=os.getenv("EMBEDDING_MODEL_NAME"))
