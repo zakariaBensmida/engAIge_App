cb5d95d218caebd2c849b23589386da16f257bc2
zakariaBensmida <45175094+zakariaBensmida@users.noreply.github.com>
2024-10-18 20:32:29 +0200

    Update query_handler.py

--- App/query_handler.py
+++ App/query_handler.py
@@ -1,7 +1,7 @@
 import os
 from transformers import pipeline
 from typing import List
-from vector_store import VectorStore  # Adjust the import path as necessary
+from .vector_store import VectorStore  # Adjust the import path as necessary
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
